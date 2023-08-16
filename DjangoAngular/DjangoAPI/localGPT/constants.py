c4f3399ce59173744bfa735acb6a0b7fa5a430ed
martigab000 <martigab000@live.com>
2023-08-16 13:11:42 -0700

    update constants and ingest

--- DjangoAngular/DjangoAPI/localGPT/constants.py
+++ DjangoAngular/DjangoAPI/localGPT/constants.py
@@ -2,16 +2,9 @@ import os
 
 # from dotenv import load_dotenv
 from chromadb.config import Settings
-# from xlxs_loader import XLSXLoader
 
 # https://python.langchain.com/en/latest/modules/indexes/document_loaders/examples/excel.html?highlight=xlsx#microsoft-excel
-from langchain.document_loaders import (
-    CSVLoader,
-    PyMuPDFLoader ,
-    TextLoader,
-    UnstructuredExcelLoader,
-    DirectoryLoader
-)
+from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader
 
 # load_dotenv()
 ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
@@ -26,21 +19,24 @@ INGEST_THREADS = os.cpu_count() or 8
 
 # Define the Chroma settings
 CHROMA_SETTINGS = Settings(
-        chroma_db_impl='duckdb+parquet',
-        persist_directory=PERSIST_DIRECTORY,
-        anonymized_telemetry=False
+    chroma_db_impl="duckdb+parquet", persist_directory=PERSIST_DIRECTORY, anonymized_telemetry=False
 )
 
 # https://python.langchain.com/en/latest/_modules/langchain/document_loaders/excel.html#UnstructuredExcelLoader
 DOCUMENT_MAP = {
     ".txt": TextLoader,
-    ".pdf": PyMuPDFLoader,
+    ".md": TextLoader,
+    ".py": TextLoader,
+    ".pdf": PDFMinerLoader,
     ".csv": CSVLoader,
     ".xls": UnstructuredExcelLoader,
-    ".xlsx": UnstructuredExcelLoader
+    ".xlsx": UnstructuredExcelLoader,
+    ".docx": Docx2txtLoader,
+    ".doc": Docx2txtLoader,
 }
 
 # Default Instructor Model
-# EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"
-# You can also choose a smaller model, don't forget to change HuggingFaceInstructEmbeddings to HuggingFaceEmbeddings in both ingest.py and run_localGPT.py
-EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
+EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"
+# You can also choose a smaller model, don't forget to change HuggingFaceInstructEmbeddings
+# to HuggingFaceEmbeddings in both ingest.py and run_localGPT.py
+# EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
--- DjangoAngular/DjangoAPI/localGPT/ingest.py
+++ DjangoAngular/DjangoAPI/localGPT/ingest.py
@@ -1,18 +1,22 @@
-import os
-from typing import List
 import logging
+import os
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 
 import click
+import torch
 from langchain.docstore.document import Document
-# from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings
+from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 
-from constants import (CHROMA_SETTINGS, DOCUMENT_MAP, EMBEDDING_MODEL_NAME, INGEST_THREADS, PERSIST_DIRECTORY,
-                       SOURCE_DIRECTORY)
-
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+from constants import (
+    CHROMA_SETTINGS,
+    DOCUMENT_MAP,
+    EMBEDDING_MODEL_NAME,
+    INGEST_THREADS,
+    PERSIST_DIRECTORY,
+    SOURCE_DIRECTORY,
+)
 
 
 def load_single_document(file_path: str) -> Document:
@@ -25,8 +29,8 @@ def load_single_document(file_path: str) -> Document:
         raise ValueError("Document type is undefined")
     return loader.load()[0]
 
-def load_document_batch(filepaths):
 
+def load_document_batch(filepaths):
     logging.info("Loading document batch")
     # create a thread pool
     with ThreadPoolExecutor(len(filepaths)) as exe:
@@ -37,25 +41,16 @@ def load_document_batch(filepaths):
         # return data and file paths
         return (data_list, filepaths)
 
-def load_documents(source_dir: str) -> List[Document]:
+
+def load_documents(source_dir: str) -> list[Document]:
     # Loads all documents from the source documents directory
     all_files = os.listdir(source_dir)
     paths = []
-    # for root, dirs, files in os.walk(source_dir):
-    #     for file_name in files:
-    #         file_path = os.path.join(root, file_name)
-    #         file_extension = os.path.splitext(file_path)[1]
-    #         if file_extension in DOCUMENT_MAP.keys():
-    #             paths.append(file_path)
-            
-    
-    # Original for loop 
     for file_path in all_files:
         file_extension = os.path.splitext(file_path)[1]
         source_file_path = os.path.join(source_dir, file_path)
         if file_extension in DOCUMENT_MAP.keys():
             paths.append(source_file_path)
-    print(paths)
 
     # Have at least one worker and at most INGEST_THREADS workers
     n_workers = min(INGEST_THREADS, max(len(paths), 1))
@@ -66,7 +61,7 @@ def load_documents(source_dir: str) -> List[Document]:
         # split the load operations into chunks
         for i in range(0, len(paths), chunksize):
             # select a chunk of filenames
-            filepaths = paths[i:(i + chunksize)]
+            filepaths = paths[i : (i + chunksize)]
             # submit the task
             future = executor.submit(load_document_batch, filepaths)
             futures.append(future)
@@ -79,14 +74,44 @@ def load_documents(source_dir: str) -> List[Document]:
     return docs
 
 
+def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
+    # Splits documents for correct Text Splitter
+    text_docs, python_docs = [], []
+    for doc in documents:
+        file_extension = os.path.splitext(doc.metadata["source"])[1]
+        if file_extension == ".py":
+            python_docs.append(doc)
+        else:
+            text_docs.append(doc)
+
+    return text_docs, python_docs
+
+
 @click.command()
 @click.option(
     "--device_type",
-    default="cuda",
+    default="cuda" if torch.cuda.is_available() else "cpu",
     type=click.Choice(
         [
-            "cpu", "cuda", "ipu", "xpu", "mkldnn", "opengl", "opencl", "ideep", "hip", "ve", "fpga", "ort",
-            "xla", "lazy", "vulkan", "mps", "meta", "hpu", "mtia",
+            "cpu",
+            "cuda",
+            "ipu",
+            "xpu",
+            "mkldnn",
+            "opengl",
+            "opencl",
+            "ideep",
+            "hip",
+            "ve",
+            "fpga",
+            "ort",
+            "xla",
+            "lazy",
+            "vulkan",
+            "mps",
+            "meta",
+            "hpu",
+            "mtia",
         ],
     ),
     help="Device to run on. (Default is cuda)",
@@ -95,22 +120,27 @@ def main(device_type):
     # Load documents and split in chunks
     logging.info(f"Loading documents from {SOURCE_DIRECTORY}")
     documents = load_documents(SOURCE_DIRECTORY)
+    text_documents, python_documents = split_documents(documents)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    texts = text_splitter.split_documents(documents)
+    python_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.PYTHON, chunk_size=1000, chunk_overlap=200
+    )
+    texts = text_splitter.split_documents(text_documents)
+    texts.extend(python_splitter.split_documents(python_documents))
     logging.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
     logging.info(f"Split into {len(texts)} chunks of text")
 
     # Create embeddings
-    # embeddings = HuggingFaceInstructEmbeddings(
-    #     model_name=EMBEDDING_MODEL_NAME,
-    #     model_kwargs={"device": device_type},
-    # )
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name=EMBEDDING_MODEL_NAME,
+        model_kwargs={"device": device_type},
+    )
     # change the embedding type here if you are running into issues.
     # These are much smaller embeddings and will work for most appications
     # If you use HuggingFaceEmbeddings, make sure to also use the same in the
     # run_localGPT.py file.
 
-    embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+    # embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
 
     db = Chroma.from_documents(
         texts,
@@ -123,6 +153,7 @@ def main(device_type):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s',
-                        level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
+    )
     main()
